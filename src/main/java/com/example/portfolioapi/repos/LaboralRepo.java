62c440316a434f45685a51273b042ec4d7a024e8
gastonferreyra944 <103758698+gastonferreyra944@users.noreply.github.com>
2022-07-27 19:01:13 -0300

    cambios clever

---
+++ src/main/java/com/example/portfolioapi/repos/LaboralRepo.java
@@ -0,0 +1,15 @@
+
+
+package com.example.portfolioapi.repos;
+
+import com.example.portfolioapi.models.LaboralModel;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/** 
+ * @author JLG
+ */
+@Repository
+public interface LaboralRepo extends CrudRepository<LaboralModel, Integer> {
+
+}
